8218b41b77dfd20b905ed080c212bf4057c2945d
sivaganeshpolepalli <sivaganeshpolepalli@localhost>
2024-01-28 14:12:38 -0500

    initial commit

---
+++ src/main/java/com/siva/payrollmanagementsystem/PayrollmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.siva.payrollmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siva/payrollmanagementsystem/PayrollmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siva.payrollmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
